29065a146e8953463a89881335e32e361cdf9643
Ramon de la Cruz Martinez <ramondelacruzmartinez@gmail.com>
2016-02-11 11:32:48 +0100

    añadida Clase TargetaPrepago a Ejercicios POO

---
+++ E.POO/src/banca/TarjetaPrepago.java
@@ -0,0 +1,77 @@
+package banca;
+
+import banca.Hora;
+import dni.DniCif;
+
+public class TarjetaPrepago {
+private String telefono;
+private Double saldo;
+//Has a, tiene un DniCif
+private DniCif dni;
+//Has a, tiene un Hora
+private Hora consumo;
+
+public TarjetaPrepago(){
+	
+}
+
+public TarjetaPrepago(String telefono, Double saldo,DniCif dni, Hora consumo){
+	this.telefono	= telefono;
+	this.saldo		= saldo;
+	this.dni		= dni;
+	this.consumo	= consumo;
+}
+
+//Getters y Setters
+public String getTelefono() {
+	return telefono;
+}
+
+public void setTelefono(String telefono) {
+	this.telefono = telefono;
+}
+
+public Double getSaldo() {
+	return saldo;
+}
+
+public void setSaldo(Double saldo) {
+	this.saldo = saldo;
+}
+
+public DniCif getDni() {
+	return dni;
+}
+
+public void setDni(DniCif dni) {
+	this.dni = dni;
+}
+
+public Hora getConsumo() {
+	return consumo;
+}
+
+public void setConsumo(Hora consumo) {
+	this.consumo = consumo;
+}
+
+//Logica
+
+void ingresarSaldo(Double cantidad){
+	this.saldo += cantidad;
+}
+
+void enviarMensaje(Integer mensajes){
+	this.saldo -= + 0.9 * mensajes;
+}
+
+void realizarLlamada(Integer segundos){
+	this.saldo -= 0.15 + (0.01 * segundos);	
+}
+void consultarTarjeta(){
+	System.out.println("Num Tel: " + this.getTelefono());
+	System.out.println("DNI: " + this.getDni());
+	System.out.println("Saldo Actual: " + this.getSaldo());
+}
+
+}
